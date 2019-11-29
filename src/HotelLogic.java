7248187f499fd7a5b628969de940590d49415154
LisaMarie1995 <57871546+LisaMarie1995@users.noreply.github.com>
2019-11-29 15:58:58 +0100

    V1.02

--- src/HotelLogic.java
+++ src/HotelLogic.java
@@ -1,3 +1,43 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+
 public class HotelLogic {
 
-}
+    private ArrayList<Room> arrListRoom = new ArrayList<Room>();
+
+        public void createRooms(){
+            if (arrListRoom.size() <= 0) {
+                int roomNumber = 0;
+                for (int i = 0; i < 11; i++) {
+                    arrListRoom.add(new Room(roomNumber, 1, 150.0, false, false));
+                    roomNumber++;
+                }
+
+
+                if (arrListRoom.size() <= 0) {
+                    for (int i = 0; i < 4; i++) {
+                        arrListRoom.add(new Room(roomNumber, 1, 200.0, true, false));
+                        roomNumber++;
+                    }
+
+
+                    if (arrListRoom.size() <= 0) {
+                        for (int i = 0; i < 5; i++) {
+                            arrListRoom.add(new Room(roomNumber, 2, 300.0, false, false));
+                            roomNumber++;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void getRooms(){
+
+            for (Room post: arrListRoom
+                 ) {
+                System.out.println(arrListRoom);
+            }
+        }
+    }
+
--- src/Room.java
+++ src/Room.java
@@ -1,12 +1,26 @@
-public class Room extends HotelLogic{
+public class Room{
     private int roomNumber;
     private int numberOfBeds;
+    private double pricePerNight;
     private boolean hasBalcony;
     private boolean isBooked;
 
-    public Room(int roomNumber, int numberOfBeds, boolean hasBalcony) {
+    @Override
+    public String toString() {
+        return "Room{" +
+                "roomNumber=" + roomNumber +
+                ", numberOfBeds=" + numberOfBeds +
+                ", pricePerNight=" + pricePerNight +
+                ", hasBalcony=" + hasBalcony +
+                ", isBooked=" + isBooked +
+                '}';
+    }
+
+    public Room(int roomNumber, int numberOfBeds, double pricePerNight, boolean hasBalcony, boolean isBooked) {
         this.roomNumber = roomNumber;
         this.numberOfBeds = numberOfBeds;
+        this.pricePerNight = pricePerNight;
         this.hasBalcony = hasBalcony;
+        this.isBooked = isBooked;
     }
 }
